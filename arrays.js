750bd1a9b97c4abcce9f85308b6417ba17b8beeb
wintercricket <hodhodhod2003@gmail.com>
2021-07-26 12:26:57 -0500

    create a scratch pad for practicing js

---
+++ arrays.js
@@ -0,0 +1,30 @@
+// let cards = [3, 9];
+// console.log(cards);
+
+
+// let cardsEl = document.getElementById("cards-el");
+// let messagesEl = document.getElementById("messages-el");
+// console.log(cardsEl);
+// cardsEl.textContent = "Cards here: " + cards.push(44);
+// messagesEl.textContent = "Messages here: " + messages;
+
+// newMessage = "Where am I?";
+// messages.push(newMessage);
+
+
+
+
+
+//for loops for counting
+let messages = [
+	" I provide free SEO evaluation on your front page",
+	" I provide sadness insurance for those who are suddenly old",
+	"The homeless family in Portland.",
+	"That made me escape and avoid, and my brother suicide."
+]
+
+
+
+for( let i = 0; i <= messages.length; i += 1){
+	console.log(messages[i]);
+}
