ae462fc5dc4a582881f3bd99a3dcdbee348e67e9
wintercricket <hodhodhod2003@gmail.com>
2021-07-27 22:35:56 -0500

    mess aroung with scratch code to learn AND logic

--- index.js
+++ index.js
@@ -7,23 +7,29 @@
 // }
 
 // let cardsEl = document.getElementById("cards-el");
-// let messagesEl = document.getElementById("messages-el");
+ // let messagesEl = document.getElementById("messages-el");
 // console.log(cardsEl);
 // cardsEl.textContent = "Cards here: " + cards.push(44);
-// messagesEl.textContent = "Messages here: " + messages;
+ //messagesEl.textContent = "Messages here: " + messages;
 
-// newMessage = "Where am I?";
-// messages.push(newMessage);
+ //messages.push(newMessage);
 
 
 
 //logical AND operators
 
-let buysSas = true;
-let sasShipped = true;
+let completeMFA = true;
+let awardMFA = true;
+let messagesEl = document.getElementById("messages-el");
 
-function generateSas(){
-	console.log("we're building your sas now");
+if(completeMFA === true && awardMFA === true){
+	generateCerticate();
+} else {
+	return messagesEl.textContent = "Your attempt is incomplete. Pay another 20k and try again.";
 }
 
-generateSas();
+function generateCertificate(){
+	messagesEl.textContent = "we're assembling your full MFA with transcipt";
+}
+
+generateCertificate();
