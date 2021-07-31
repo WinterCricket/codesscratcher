9b956cf102e012f00086b0835a574551314eb671
wintercricket <hodhodhod2003@gmail.com>
2021-07-31 17:37:05 -0500

    use my own solution for the conditional ex

--- index.js
+++ index.js
@@ -17,47 +17,47 @@
 // let solvEl = document.getElementById("solv-el");
 
 // //logical AND operators
-// let hasSolved = true;
-// let hasHintsLeft = true;
-// let message = "You're an idiot, so we're giving you the solution now...";
-// function giveStudentSolution(){
-// 	if(hasSolved === false && hasHintsLeft === false){
-// 		solvEl.textContent = message;
-// 	} else if(hasSolved === false && hasHintsLeft || hasSolved && hasHintsLeft === false){
-// 		solvEl.textContent = "You're almost there. Keep trying.";
-// 	} else {
-// 		solvEl.textContent = "So, smart ass, you've solved it. Now what?";
-// 	}
-// }
-// giveStudentSolution();
-
-
-
-function convertToRoman(num) {
-   const romanToArabic = {
-        M:1000,
-        CM:900,
-        D:500,
-        CD:400,
-        C:100,
-        XC:90,
-        L:50,
-        XL:40,
-        X:10,
-        IX:9,
-        V:5,
-        IV:4,
-        I:1
-    };
-    let romantic = "";
-    for(let key in romanToArabic){
-        romanToArabic[key];
-        while(num >= romanToArabic[key]){
-            romantic += key;
-            num -= romanToArabic[key];
-        }
-    }
- return romantic;
+let hasSolved = true;
+let hasHintsLeft = true;
+let message = "You're an idiot, so we're giving you the solution now...";
+function giveStudentSolution(){
+	if(hasSolved === false && hasHintsLeft === false){
+		return message;
+	} else if(hasSolved === false && hasHintsLeft || hasSolved && hasHintsLeft === false){
+		return "You're almost there. Keep trying.";
+	} else {
+		return "So, smart ass, you've solved it. Now what?";
+	}
 }
+messagesEl.textContent = giveStudentSolution();
 
-messagesEl.textContent = "Translate Arabic to Roman: "+convertToRoman(2021);
+
+
+// function convertToRoman(num) {
+//    const romanToArabic = {
+//         M:1000,
+//         CM:900,
+//         D:500,
+//         CD:400,
+//         C:100,
+//         XC:90,
+//         L:50,
+//         XL:40,
+//         X:10,
+//         IX:9,
+//         V:5,
+//         IV:4,
+//         I:1
+//     };
+//     let romantic = "";
+//     for(let key in romanToArabic){
+//         romanToArabic[key];
+//         while(num >= romanToArabic[key]){
+//             romantic += key;
+//             num -= romanToArabic[key];
+//         }
+//     }
+//  return romantic;
+// }
+
+// messagesEl.textContent = "Translate Arabic to Roman: "+convertToRoman(2021);
