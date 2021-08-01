42ba42bc277bb5057f31f7da8f2a8708f50647de
wintercricket <hodhodhod2003@gmail.com>
2021-07-31 21:56:39 -0500

    my own version attempt of OR movie conditional

--- index.js
+++ index.js
@@ -17,19 +17,19 @@
 // let solvEl = document.getElementById("solv-el");
 
 // //logical AND operators
-let hasSolved = true;
-let hasHintsLeft = true;
-let message = "You're an idiot, so we're giving you the solution now...";
-function giveStudentSolution(){
-	if(hasSolved === false && hasHintsLeft === false){
-		return message;
-	} else if(hasSolved === false && hasHintsLeft || hasSolved && hasHintsLeft === false){
-		return "You're almost there. Keep trying.";
-	} else {
-		return "So, smart ass, you've solved it. Now what?";
-	}
-}
-messagesEl.textContent = giveStudentSolution();
+// let hasSolved = false;
+// let hasHintsLeft = false;
+// let message = "You're an idiot, so we're giving you the solution now...";
+// function giveStudentSolution(){
+// 	if(hasSolved === false && hasHintsLeft === false){
+// 		return message;
+// 	} else if(hasSolved === false && hasHintsLeft || hasSolved && hasHintsLeft === false){
+// 		return "You're almost there. Keep trying.";
+// 	} else {
+// 		return "So, smart ass, you've solved it. Now what?";
+// 	}
+// }
+// messagesEl.textContent = giveStudentSolution();
 
 
 
@@ -61,3 +61,15 @@ messagesEl.textContent = giveStudentSolution();
 // }
 
 // messagesEl.textContent = "Translate Arabic to Roman: "+convertToRoman(2021);
+
+let likeDocs = false;
+let likeStart = false;
+
+function recommendFilm(){
+	if(likeDocs ===true ||likeStart === true){
+		return "Watch Star Wars when you're sick and tired of your job.";
+	} else {
+		 return "I've nothing to recommend, except that you grow up.";
+	}
+}
+messagesEl.textContent = recommendFilm();
