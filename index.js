4ce4847ab56bd9a0c838ac0cea6077434659ed8f
wintercricket <hodhodhod2003@gmail.com>
2021-08-03 20:08:40 -0500

    conditional logic

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
-// let cards = [3, 9];
-
-// cards.push(5)
-
-// for (let i = 0; i <= cards.length; i ++ 1) {
-// 	console.log(cards[i]);
-// }
-
-// let cardsEl = document.getElementById("cards-el");
-// let largestCountries = [];
-
-
-
-
-let largestCountries = ["Doo", "India", "USA", "Indonesia", "Daa"];
-largestCountries.shift();
-largestCountries.unshift("China");
-largestCountries.pop();
-largestCountries.push("Pakistan");
+let dayOfMonth = 31;
+let weekday = "Friday";
 let messagesEl = document.getElementById("messages-el");
-for(let i = 0; i < largestCountries.length; i++){
-
-	messagesEl.textContent += "- "+largestCountries[i];
-	//largestCountries.shift();
-	
-
+if(dayOfMonth === 13 && weekday === "Friday"){
+	messagesEl.textContent = "Boooooo!";
+} else{
+	messagesEl.textContent = "Good day!";
 }
-
 
