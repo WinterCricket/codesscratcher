bae05ebdab3e38eacfdbc424b4c32773f9122745
wintercricket <hodhodhod2003@gmail.com>
2021-08-04 15:35:25 -0500

    settle on simpler sort app

--- index.js
+++ index.js
@@ -13,55 +13,43 @@
 // }
 // showHand();
 
+
+
+
+
 let fruit = ["orange", "red", "red","blue", "orange", "red", "orange","blue"];
 
+		
+	 fruit.push("pink");
+	// fruit.push("black");
+	// fruit.push("yellow");
+	// fruit.push("blue");
 
 
-
-
-
-
-
-console.log(fruit);
 let redShelf = document.getElementById("red-shelf");
 let orangeShelf = document.getElementById("orange-shelf");
 let blueShelf = document.getElementById("blue-shelf");
 let miscShelf = document.getElementById("misc-shelf");
 
+function sortColors(){
 for (let i = 0; i < fruit.length; i++){
 	if(fruit[i] === "orange"){
 		orangeShelf.textContent += fruit[i] +"  ";
 	}
-	 else if(fruit[i] === "red"){
-	redShelf.textContent += fruit[i] + "  ";
+	else if(fruit[i] === "red"){
+		redShelf.textContent += fruit[i] + "  ";
 	}
 	else if (fruit[i] === "blue"){
 		blueShelf.textContent += fruit[i] + "  ";
 	} else {
 		
-		miscShelf.textContent = fruit[i] +"  ";
+			miscShelf.textContent += fruit[i]+ "  ";
+		
+		
 	}
 	
+	}
+
 }
 
-function addFruit(){
-	fruit.push("black");
-	miscShelf.textContent = fruit;
-}
-
-
-
-	
-	
-
-
-
-// const fruits = ["Banana", "Orange", "Apple", "Mango"];
-// document.getElementById("demo");
-// let text = "<ul>";
-// fruits.forEach(myFunction);
-// document.getElementById("demo").innerHTML = text;
-// text += "<ul>";
-//  function myFunction(val){
-//  	text += "<li>" + val + "</li>";
-//  }
+sortColors();
