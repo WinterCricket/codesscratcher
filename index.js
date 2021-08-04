adc4d09a948a299916aaf4b1f068b7bb14ed6722
wintercricket <hodhodhod2003@gmail.com>
2021-08-03 22:00:15 -0500

    sorting red and orange into separate piles out works console

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 // rock, paper, scissors
-let hands = ["rock", "paper", "scissors"];
+// let hands = ["rock", "paper", "scissors"];
 
-messagesEl = document.getElementById("messages-el");
+// messagesEl = document.getElementById("messages-el");
 
-function showHand(){
+// function showHand(){
 
-let randomNumber = Math.floor(Math.random()*3);
+// let randomNumber = Math.floor(Math.random()*3);
 
-messagesEl.textContent = hands[randomNumber];
+// messagesEl.textContent = hands[randomNumber];
 
 
+// }
+// showHand();
+
+let fruit = ["orange", "red", "red", "orange", "red", "orange"];
+
+let redShelf = document.getElementById("red-shelf");
+let orangeShelf = document.getElementById("orange-shelf");
+
+
+
+
+
+for (let i = 0; i < fruit.length; i++){
+	if(fruit[i] === "orange"){
+		console.log(fruit[i]);
+	}
+	 else {
+		console.log(fruit[i]);
+	}
 }
-showHand();
