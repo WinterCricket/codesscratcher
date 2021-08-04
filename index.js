baea95f19aea0fdcc8d0a6c60c91b09e21b50481
wintercricket <hodhodhod2003@gmail.com>
2021-08-04 11:23:08 -0500

    try list using innerHTML

--- index.js
+++ index.js
@@ -13,27 +13,38 @@
 // }
 // showHand();
 
-let fruit = ["orange", "red", "red","blue", "orange", "red", "orange","blue"];
+// let fruit = ["orange", "red", "red","blue", "orange", "red", "orange","blue"];
 
-let redShelf = document.getElementById("red-shelf");
-let orangeShelf = document.getElementById("orange-shelf");
-let blueShelf = document.getElementById("blue-shelf");
-let miscShelf = document.getElementById("misc-shelf");
+// let redShelf = document.getElementById("red-shelf");
+// let orangeShelf = document.getElementById("orange-shelf");
+// let blueShelf = document.getElementById("blue-shelf");
+// let miscShelf = document.getElementById("misc-shelf");
 
-fruit[0] = "green";
-fruit[fruit.length] = "purple";
-console.log(fruit);
+// fruit[0] = "green";
+// fruit[fruit.length] = "purple";
+// fruit.push("yellow");
 
-for (let i = 0; i < fruit.length; i++){
-	if(fruit[i] === "orange"){
-		orangeShelf.textContent += fruit[i] +"  ";
-	}
-	 else if(fruit[i] === "red"){
-	redShelf.textContent += fruit[i] + "  ";
-	}
-	else if (fruit[i] === "blue"){
-		blueShelf.textContent += fruit[i] + "  ";
-	} else {
-		miscShelf.textContent += fruit[i] +"  ";
-	}
+
+// for (let i = 0; i < fruit.length; i++){
+// 	if(fruit[i] === "orange"){
+// 		orangeShelf.textContent += fruit[i] +"  ";
+// 	}
+// 	 else if(fruit[i] === "red"){
+// 	redShelf.textContent += fruit[i] + "  ";
+// 	}
+// 	else if (fruit[i] === "blue"){
+// 		blueShelf.textContent += fruit[i] + "  ";
+// 	} else {
+// 		miscShelf.textContent += fruit[i] +"  ";
+// 	}
+// }
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+demoEl = document.getElementById("demo");
+let text = "<ul>";
+for(let i = 0; i < fruits.length; i++){
+	text += "<li>" + fruits[i] + "</li>";
 }
+text += "<ul>";
+
+demoEl.innerHTML = text;//need to use innerHTML instead of textContent
