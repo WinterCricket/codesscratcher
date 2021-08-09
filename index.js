cbe2c49d9454b4b5ae3b40a21dc6748232b50c47
wintercricket <hodhodhod2003@gmail.com>
2021-08-09 16:06:23 -0500

    add another variable

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 
 
-const recipient = "Lordly Fly Opener"
-const email = `
-Hi, ${recipient}! What are you doing?`
 
-console.log(email)
+
+const greetEl = document.getElementById("greet-el")
+
+const recipient = "Lordly Fly Opener"
+const sender = "Traveler Home"
+const email = `
+	Hi, ${recipient}! What are you doing? Bonzai, 
+		${sender}!
+`
+
+greetEl.textContent = email
