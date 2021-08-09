ed6f7282144a64817a60c9a447c8dd38918f21fa
wintercricket <hodhodhod2003@gmail.com>
2021-08-09 16:16:51 -0500

    log out multiline

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const greetEl = document.getElementById("greet-el")
 const recipient = "Lordly Fly Opener"
 const sender = "Traveler Home"
 const email = `
-	Hi, ${recipient}! What are you doing? Bonzai, 
-		${sender}!
+	Hi, ${recipient}! 
+	What are you doing?
+	Bonzai, ${sender}!
 `
-
+console.log(email)
 greetEl.textContent = email
