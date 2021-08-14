01b7d435259c35e0e8c504c8767187aa71470f23
wintercricket <hodhodhod2003@gmail.com>
2021-08-14 18:18:42 -0500

    dom out array via toString method

--- index.js
+++ index.js
@@ -3,21 +3,14 @@
 
 const domEl = document.getElementById("dom-el")
 
-const player = "Selfsame"
-const opponent = "Otherness"
-const game = "NonviolentCompete"
-let points = 0
-let hasWon = false
+const myCourses = ["Creative Writing for Geniuses", "Coding for Literary Minds ", "UI Design Fundamentals "]
+const yourCourses = ["Pucker Up: How to Succeed ", "Business Cycles ", "Make Artists Cry "]
 
+function listCourses(arr){
 
-
-
-
-points = Math.floor(Math.random() * 300)
-console.log(points)
-
-if(points > 99){
-    domEl.textContent = `${player} earned ${points} to win the ${game} game!`
-} else {
-    domEl.textContent = `The winner of ${game} is ${opponent}, who earned ${points}!  ${player} has lost.`
+    arr = arr.toString()
+    domEl.textContent = arr
 }
+
+
+listCourses(myCourses)
