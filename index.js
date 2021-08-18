accd97a2d65c65255d1a3503cf5715c4e910f3b0
wintercricket <hodhodhod2003@gmail.com>
2021-08-17 20:35:42 -0500

    score rendering button using foreach

--- index.js
+++ index.js
@@ -8,14 +8,27 @@ const domEl = document.getElementById("dom-el")
 // const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 // domEl.textContent = leadsFromLocalStorage
+const scoreBtn = document.getElementById("score-btn")
 
 
-const flog = JSON.parse(localStorage.getItem("writeCourses"))
-let listItems = ""
-for(let i=0; i<flog.length; i++){
-    listItems +=`<li>
-        ${flog[i]}
-    </li>
-    `
-}
-domEl.innerHTML = listItems
+
+let data = [
+
+    {
+        player: "Jane",
+        score: 52
+    },
+
+
+    {
+        player: "Mark",
+        score: 48
+    }
+]
+
+scoreBtn.addEventListener("click", function() {
+    
+    const jane = data[0].score
+    domEl.textContent = jane
+    
+})
